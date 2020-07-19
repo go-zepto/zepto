60b62f16ede52ff6cb73e214c4edf824bfd02e92
Carlos Strand <carlosstrand@gmail.com>
2020-07-19 01:31:58 -0300

    Add assert methods and docs

--- web/testutils.go
+++ web/testutils.go
@@ -5,6 +5,7 @@ import (
 	"github.com/stretchr/testify/assert"
 	"io"
 	"io/ioutil"
+	"net/http"
 	"net/http/httptest"
 	"testing"
 )
@@ -21,9 +22,19 @@ func NewZeptoTest(t *testing.T, app *App) *ZeptoTest {
 	}
 }
 
+type TestHandlerRequestOptions struct {
+	Handler        RouteHandler
+	Method         string
+	Target         string
+	Body           io.Reader
+	InitialSession map[string]string
+}
+
 type TestHandlerRequestResult struct {
 	t      *testing.T
+	Ctx    *DefaultContext
 	Status int
+	Header http.Header
 	Body   string
 }
 
@@ -39,15 +50,27 @@ func (tr *TestHandlerRequestResult) AssertBodyContains(str string) {
 	assert.Contains(tr.t, tr.Body, str)
 }
 
-func (zt *ZeptoTest) TestHandlerRequest(h RouteHandler, m string, t string, b io.Reader) (TestHandlerRequestResult, error) {
-	req := httptest.NewRequest(m, t, b)
+func (tr *TestHandlerRequestResult) AssertSessionValue(key string, value string) {
+	assert.Equal(tr.t, value, tr.Ctx.session.Get(key))
+}
+
+func (tr *TestHandlerRequestResult) AssertHeaderValue(key string, value string) {
+	assert.Equal(tr.t, value, tr.Header.Get(key))
+}
+
+func (zt *ZeptoTest) TestHandlerRequest(opts TestHandlerRequestOptions) (TestHandlerRequestResult, error) {
+	req := httptest.NewRequest(opts.Method, opts.Target, opts.Body)
 	res := httptest.NewRecorder()
 	ctx := NewDefaultContext()
+	ctx.session = zt.app.getSession(res, req)
 	ctx.req = req
 	ctx.res = res
 	ctx.logger = log.New()
 	ctx.tmplEngine = zt.app.tmplEngine
-	err := h(ctx)
+	for key, value := range opts.InitialSession {
+		ctx.session.Set(key, value)
+	}
+	err := opts.Handler(ctx)
 	if err != nil {
 		return TestHandlerRequestResult{}, err
 	}
@@ -55,7 +78,9 @@ func (zt *ZeptoTest) TestHandlerRequest(h RouteHandler, m string, t string, b io
 	body, _ := ioutil.ReadAll(resp.Body)
 	return TestHandlerRequestResult{
 		t:      zt.t,
+		Ctx:    ctx,
 		Status: resp.StatusCode,
+		Header: resp.Header,
 		Body:   string(body),
 	}, nil
 }
--- web/testutils_test.go
+++ web/testutils_test.go
@@ -1,39 +1,101 @@
 package web
 
 import (
-	"fmt"
 	"testing"
 )
 
-func HelloHandler(ctx Context) error {
-	ctx.SetStatus(201)
-	return ctx.RenderJson(map[string]string{"hello": "world"})
-}
-
-func TmplHandler(ctx Context) error {
-	ctx.SetStatus(201)
-	return ctx.Render("hello")
-}
-
 func TestHandlerRequestJson(t *testing.T) {
+	var HelloHandler = func(ctx Context) error {
+		return ctx.RenderJson(map[string]string{"hello": "world"})
+	}
 	app := NewApp()
 	zt := NewZeptoTest(t, app)
-	res, err := zt.TestHandlerRequest(HelloHandler, "GET", "/", nil)
+	res, err := zt.TestHandlerRequest(TestHandlerRequestOptions{
+		Handler: HelloHandler,
+		Method:  "GET",
+		Target:  "/",
+		Body:    nil,
+	})
 	if err != nil {
 		t.Fatal(err)
 	}
-	res.AssertStatusCode(201)
 	res.AssertBodyEquals("{\"hello\":\"world\"}\n")
-	fmt.Println(res)
 }
 
 func TestHandlerRequestTemplate(t *testing.T) {
+	var TmplHandler = func(ctx Context) error {
+		return ctx.Render("hello")
+	}
 	app := NewApp()
 	zt := NewZeptoTest(t, app)
-	res, err := zt.TestHandlerRequest(TmplHandler, "GET", "/", nil)
+	res, err := zt.TestHandlerRequest(TestHandlerRequestOptions{
+		Handler: TmplHandler,
+		Method:  "GET",
+		Target:  "/",
+		InitialSession: map[string]string{
+			"abc": "123",
+		},
+	})
 	if err != nil {
 		t.Fatal(err)
 	}
-	res.AssertStatusCode(201)
 	res.AssertBodyContains("This is a template used for tests")
+}
+
+func TestHandlerRequestStatus(t *testing.T) {
+	var handler = func(ctx Context) error {
+		ctx.SetStatus(401)
+		return ctx.RenderJson(map[string]string{"hello": "world"})
+	}
+	app := NewApp()
+	zt := NewZeptoTest(t, app)
+	res, err := zt.TestHandlerRequest(TestHandlerRequestOptions{
+		Handler: handler,
+		Method:  "GET",
+		Target:  "/",
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+	res.AssertStatusCode(401)
+}
+
+func TestHandlerRequestHeader(t *testing.T) {
+	var handler = func(ctx Context) error {
+		ctx.Response().Header().Set("X-HELLO", "WORLD")
+		return ctx.RenderJson(map[string]string{"hello": "world"})
+	}
+	app := NewApp()
+	zt := NewZeptoTest(t, app)
+	res, err := zt.TestHandlerRequest(TestHandlerRequestOptions{
+		Handler: handler,
+		Method:  "GET",
+		Target:  "/",
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+	res.AssertHeaderValue("X-HELLO", "WORLD")
+}
+
+func TestHandlerRequestSession(t *testing.T) {
+	var handler = func(ctx Context) error {
+		ctx.Session().Set("user_id", "42")
+		return ctx.RenderJson(map[string]string{"hello": "world"})
+	}
+	app := NewApp()
+	zt := NewZeptoTest(t, app)
+	res, err := zt.TestHandlerRequest(TestHandlerRequestOptions{
+		Handler: handler,
+		Method:  "GET",
+		Target:  "/",
+		InitialSession: map[string]string{
+			"initial_session": "ok",
+		},
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+	res.AssertSessionValue("initial_session", "ok")
+	res.AssertSessionValue("user_id", "42")
 }
